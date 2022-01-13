54572323370f3d340b69a58fd6011c85b0d24f93
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-01-13 14:07:34 -0300

    component question

---
+++ componetns/Questao.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/Questao.module.css'
+import QuestaoModel from "../model/questao";
+
+interface QuestaoProps {
+    valor: QuestaoModel
+}
+
+export default function Questao(props) {
+    const questao = props.valor 
+
+    return(
+        <div className={styles.questao}>
+            <h1>Questao</h1>
+
+        </div>
+    )
+
+}
