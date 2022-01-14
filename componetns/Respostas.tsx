6b9a01b88eb432639b3494b02a9a3100cadf9491
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-01-14 13:14:40 -0300

    Response component #7

--- componetns/Respostas.tsx
+++ componetns/Respostas.tsx
@@ -16,20 +16,34 @@ export default function Repsostas(props: RespostaProps) {
     const resposta = props.valor
     return (
         <div className={styles.resposta}
-        onClick={() => props.respostaFornecida(props.indice)} >
+         onClick={() => props.respostaFornecida(props.indice)} >
             <div className={styles.conteudoResposta}>
-            <div className={styles.frente}>
-                <div className={styles.letra} 
-                style={{backgroundColor: props.corletra}}>
-                    {props.letra}
+                {!resposta.revelada ? (
+                <div className={styles.frente}>
+                    <div className={styles.letra} 
+                    style={{backgroundColor: props.corletra}}>
+                        {props.letra}
+                    </div>
+                    <div className={styles.valor}>
+                        {resposta.valor}
+                    </div>
+                 </div> 
+                ) : (
+                <div className={styles.verso}>
+                    {resposta.certa ? (
+                    <div className={styles.certa}>
+                        <div>A Resposta correta é...</div>
+                        <div className={styles.valor}>{resposta.valor}</div>
+                    </div>
+                    ) : (
+                    <div className={styles.errada}>
+                        <div>A Resposta Informada está Errada...</div>
+                        <div className={styles.valor}>{resposta.valor}</div>
+                    </div>
+                    )}
                 </div>
-                <div className={styles.valor}>
-                    {resposta.valor}
-                </div>
-            </div>
-            <div className={styles.verso}>
-            </div>
 
+                )}
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,13 @@ const questaoMock = new QuestaoModel(1, "Melhor cor?", [
   RespostasModel.errada("Verde"),
   RespostasModel.errada("Vermelha"),
   RespostasModel.errada("Azul"),
-  RespostasModel.errada("Preta"),
+  RespostasModel.certa("Preta"),
 ]);
 
 export default function Home() {
   const [questao, setQeustao] = useState(questaoMock)
   
   function  respostaFornecida(indice: number) {
-    console.log(indice)
     setQeustao(questao.responderCom(indice))
   }
 
