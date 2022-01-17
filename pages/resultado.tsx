1f8ec39761f6516df79c365d22527d9199958df7
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-01-17 16:18:32 -0300

    Result component

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,16 +1,20 @@
+import styles from '../styles/Resultados.module.css'
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function resultado() {
+    const router = useRouter()
+
+    const total = +router.query.total
+    const certas = +router.query.certas
+    const percentual = Math.round((certas / total ) * 100)
+
     return (
-        <div>
-            <h1>Resultado!</h1>
-            <div>
-                <button>
-                    <Link href='/'> 
-                    <a>Voltar</a>
-                    </Link>
-                </button>
-            </div>
+        <div className={styles.resultados}>
+            <h1>Resuoltado Final</h1>
+            <div>{total}</div>
+            <div>{certas}</div>
+            <div>{`${percentual}%`}</div>
         </div>
     )
 }
