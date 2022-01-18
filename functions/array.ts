1dd2a13d3fe9be674e8fd5a9537147a16259ec9b
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-01-18 12:13:12 -0300

    Fixed error

--- functions/array.ts
+++ functions/array.ts
@@ -1,4 +1,4 @@
-export function embarralhar(elementos: Math. any[]): any[] {
+export function embarralhar(elementos: any[]): any[] {
     return elementos
         .map(valor => ({ valor, aleatorio: Math.random() }))
         .sort((obj1, obj2) => obj1.aleatorio - obj2.aleatorio)
--- pages/api/questao/[id].ts
+++ pages/api/questao/[id].ts
@@ -7,7 +7,7 @@ export default function handler(req, res) {
   const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
 
   if(unicaQuestaoOuNada.length === 1) {
-    const questaoSelecionada = unicaQuestaoOuNada[0]
+    const questaoSelecionada = unicaQuestaoOuNada[0].embarralharRespostas()
     res.status(200).json(questaoSelecionada.toObject())
   } else {
     res.status(204).send()
