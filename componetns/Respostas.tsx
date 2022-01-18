c95a52b45fd2d9a30f63f3ae6d97ab225aa148a0
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-01-18 10:02:15 -0300

    response animation

--- componetns/Respostas.tsx
+++ componetns/Respostas.tsx
@@ -14,11 +14,11 @@ interface RespostaProps{
 
 export default function Repsostas(props: RespostaProps) {
     const resposta = props.valor
+    const respostaRevelada = resposta.revelada ? styles.respostaRevelada : ''
     return (
         <div className={styles.resposta}
          onClick={() => props.respostaFornecida(props.indice)} >
-            <div className={styles.conteudoResposta}>
-                {!resposta.revelada ? (
+            <div className={`${respostaRevelada} ${styles.conteudoResposta}`}>  
                 <div className={styles.frente}>
                     <div className={styles.letra} 
                     style={{backgroundColor: props.corletra}}>
@@ -27,8 +27,7 @@ export default function Repsostas(props: RespostaProps) {
                     <div className={styles.valor}>
                         {resposta.valor}
                     </div>
-                 </div> 
-                ) : (
+                 </div>                 
                 <div className={styles.verso}>
                     {resposta.certa ? (
                     <div className={styles.certa}>
@@ -42,8 +41,6 @@ export default function Repsostas(props: RespostaProps) {
                     </div>
                     )}
                 </div>
-
-                )}
             </div>
         </div>
     )
